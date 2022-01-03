62eefc4c679bb662a76005c8c52e00ecdd1ec458
html-courses <74095748+html-courses@users.noreply.github.com>
2022-01-03 21:13:35 +0200

    Add files via upload

---
+++ dowlnloads/linux.php
@@ -0,0 +1,18 @@
+<?php
+include("./filemain.php");
+include("../header.php");
+?>
+
+<body>
+<h1>Как да инсталираме SnackOS на Linux!<
+</h1>
+
+<h4>1. Отваряте Google Chrome</h4>
+<br />
+<h4>2. Отваряте SnackOS</h4>
+<img src="./img/opened.png" width="808em"><br />
+<h4>3. Кликате на трите точки и следвате стъпките върху снимката</h4>
+<img src="./img/ubuntu.png"><br />
+<br />
+<h4>И таке вече сте готови!</h4>
+</body>
---
+++ dowlnloads/windows.php
@@ -0,0 +1,31 @@
+<?php
+include("./filemain.php");
+include("../header.php");
+?>
+
+<body>
+<h1>Как да инсталирате Snack OS на вашия компютър!</h1>
+
+<h4>1. Отваряте уебсайта:</h4>
+<img src="./img/opened.png" width="808em">
+<br />
+<br />
+<h4>2. Кликате на трите точки</h4>
+<img src="./img/3-dots.png" width="808em">
+<br />
+<br />
+<h4>3. Кликате на "Още инструменти"</h4>
+<img src="./img/other.png" width="808em">
+<br />
+<br />
+<h4>4. След това натискате на "Създаване на пряк път..."</h4>
+<img src="./img/creating.png" width="808em">
+<br />
+<br />
+<h4>5. Следвате стъпките и сте готови!</h4>
+<img src="./img/shortcut.png" width="808em">
+<br />
+<br />
+<h4>Вече приложението е на вашия работен плот!</h4>
+<img src="ready.png" width="108em">
+</body>
